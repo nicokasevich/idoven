3784039decccacb630535834de6791fbad20ee99
Nicolas Kasevich <n.kasevich@gmail.com>
2024-03-08 17:22:48 +0100

    Added setting from dotenv

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./sql_app.db"
+from app.core.settings import settings
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
